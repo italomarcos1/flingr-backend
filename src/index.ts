e88df045bbff425dc9a33421249939ce037492ce
Italo <italomarcos0010@gmail.com>
2024-05-24 16:14:28 -0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata"
+
+import path from "path"
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql"
+import { UserResolver } from "./resolver/UserResolver";
+
+(async () => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql')
+  });
+
+  const server = new ApolloServer({ schema })
+
+  const { url } = await server.listen();
+
+  console.log(`Running on ${url}`)
+})()
