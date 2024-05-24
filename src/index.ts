9598852648f6041092c2a4f23a7eba9202926712
Italo <italomarcos0010@gmail.com>
2024-05-24 16:38:33 -0300

    websocket server all set

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,8 @@ import express from 'express';
 import { createServer } from 'http';
 
 // import { makeExecutableSchema } from '@graphql-tools/schema';
-// import { WebSocketServer } from 'ws';
-
-// import { useServer } from 'graphql-ws/lib/use/ws';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 import { buildSchema } from "type-graphql"
 import { UserResolver } from "./resolver/UserResolver";
@@ -23,24 +22,39 @@ app.use(express.json());
 
 const httpServer = createServer(app);
 
-
 (async () => {
   const schema = await buildSchema({
     resolvers: [UserResolver],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql')
   });
 
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/',
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer);
+
   const server = new ApolloServer({
     schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })]
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+      ApolloServerPluginLandingPageLocalDefault({ embed: true })
+    ]
   })
 
   await server.start();
   
-  server.applyMiddleware(({
-    app,
-    path: '/',
-  }))
+  server.applyMiddleware(({ app, path: '/' }))
 
   await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
